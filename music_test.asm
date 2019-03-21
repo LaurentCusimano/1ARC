8d0099e3884482f55a38e7d66be97981d2ab0db8
Kentin <296188@supinfo.com>
2019-03-21 12:35:47 +0100

    add music_test

---
+++ music_test.asm
@@ -0,0 +1,32 @@
+   ORG 100h
+        mov     al, 182         ; Prepare the speaker for the
+        out     43h, al         ;  note.
+        mov     ax, 4560        ; Frequency number (in decimal)
+                                ;  for middle C.
+        out     42h, al         ; Output low byte.
+        mov     al, ah          ; Output high byte.
+        out     42h, al 
+        in      al, 61h         ; Turn on note (get value from
+                                ;  port 61h).
+        or      al, 00000011b   ; Set bits 1 and 0.
+        out     61h, al         ; Send new value.
+        mov     bx, 25          ; Pause for duration of note.
+pause1:
+        mov     cx, 65535
+pause2:
+        dec     cx
+        jne     pause2
+        dec     bx
+        jne     pause1
+        in      al, 61h         ; Turn off note (get value from
+                                ;  port 61h).
+        and     al, 11111100b   ; Reset bits 1 and 0.
+   
+   
+        out     61h, al         ; Send new value.
+        
+        
+        
+        ret 
+        
+        END
