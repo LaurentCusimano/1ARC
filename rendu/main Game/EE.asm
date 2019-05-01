a808f6882b5a4b6085ef50cb6a83d1427a62fcee
Kentin <296188@supinfo.com>
2019-05-01 18:20:52 +0200

    Rendu

---
+++ rendu/main Game/EE.asm
@@ -0,0 +1,65 @@
+;draw 13 eggs in the maze:
+mov dl,7
+mov dh,4
+call SetCursor
+PRINT 248 
+
+mov dl,11
+mov dh,12
+call SetCursor
+PRINT 248
+
+mov dl,19
+mov dh,2
+call SetCursor
+PRINT 248
+
+mov dl,23
+mov dh,11
+call SetCursor
+PRINT 248
+
+mov dl,24
+mov dh,17
+call SetCursor
+PRINT 248
+
+mov dl,31
+mov dh,13
+call SetCursor
+PRINT 248
+
+mov dl,36
+mov dh,2
+call SetCursor
+PRINT 248
+
+mov dl,38
+mov dh,17
+call SetCursor
+PRINT 248
+
+mov dl,39
+mov dh,21
+call SetCursor
+PRINT 248
+
+mov dl,41
+mov dh,8
+call SetCursor
+PRINT 248
+
+mov dl,44
+mov dh,15
+call SetCursor
+PRINT 248
+
+mov dl,47
+mov dh,2
+call SetCursor
+PRINT 248
+
+mov dl,57
+mov dh,10
+call SetCursor
+PRINT 248
---
+++ rendu/main Game/maze_test.asm
@@ -0,0 +1,207 @@
+ 
+      draw_inv: 
+      
+        mov dl,63
+        mov dh,-1
+        draw_leftinv:
+        
+            add dh,1
+            call SetCursor       
+            PRINT 186 
+            cmp dh,4
+            je draw_downinv_p1
+            loop draw_leftinv
+        
+        
+        draw_downinv_p1:
+        add dh,1
+        call SetCursor       
+        PRINT '-'
+        
+        
+        draw_downinv:
+            add dl,1
+            call SetCursor      
+            PRINT '-' 
+            cmp dl,75
+            je draw_rightinv
+           loop draw_downinv
+        
+        draw_rightinv:
+            sub dh,1
+            call SetCursor       
+            PRINT 186 
+            cmp dh,0
+            je draw_textinv
+           loop draw_rightinv
+           
+          draw_textinv:
+                mov dl,65
+                mov dh,2
+                call SetCursor       
+                PRINT 'Inventory'
+            
+           
+          
+                
+                
+                
+                
+     
+  draw_object_redzone:
+        ;object(1stkey RED) spawn draw 
+            draw_redkey:
+            ;cursor pos:
+            mov dl,13 
+            mov dh,21
+            mov bh, 0
+            mov ah, 0x2
+            int 0x10
+            mov cx, 1 ; nb char
+            mov bh, 0
+            mov bl, 0x40 ; color
+            mov al, 0x20 ; blank char
+            mov ah, 0x9
+            int 0x10 
+            mov ah, 02h
+            mov bh, 00
+            int 10h
+            PRINT 216
+
+             
+    
+            draw_reddoor:
+                ;object(door) spawn draw 
+                ;cursor pos:
+                mov dl,14 
+                mov dh,17
+                mov bh, 0
+                mov ah, 0x2
+                int 0x10
+                mov cx, 1 ; nb char
+                mov bh, 0
+                mov bl, 0x40 ; color
+                mov al, 0x20 ; blank char
+                mov ah, 0x9
+                int 0x10 
+                mov ah, 02h
+                mov bh, 00
+                int 10h 
+                PRINT 177
+
+       draw_object_purplezone:
+   
+            draw_purpledoor:
+                ;object(door) spawn draw 
+                ;cursor pos:
+                mov dl,15 
+                mov dh,10
+                mov bh, 0
+                mov ah, 0x2
+                int 0x10
+                mov cx, 1 ; nb char
+                mov bh, 0
+                mov bl, 0x50 ; color
+                mov al, 0x20 ; blank char
+                mov ah, 0x9
+                int 0x10 
+                mov ah, 02h
+                mov bh, 00
+                int 10h 
+                PRINT 177
+             
+                                       
+         draw_object_yellowzone:
+   
+            draw_yellowdoor:
+                ;object(door) spawn draw 
+                ;cursor pos:
+                mov dl,24 
+                mov dh,6
+                mov bh, 0
+                mov ah, 0x2
+                int 0x10
+                mov cx, 1 ; nb char
+                mov bh, 0
+                mov bl, 0x30 ; color
+                mov al, 0x20 ; blank char
+                mov ah, 0x9
+                int 0x10 
+                mov ah, 02h
+                mov bh, 00
+                int 10h 
+                PRINT 177
+    
+          draw_object_OrangeANDGreenzone:
+   
+            draw_Orangedoor:
+                ;object(door) spawn draw 
+                ;cursor pos:
+                mov dl,51 
+                mov dh,19
+                mov bh, 0
+                mov ah, 0x2
+                int 0x10
+                mov cx, 1 ; nb char
+                mov bh, 0
+                mov bl, 0x60 ; color
+                mov al, 0x20 ; blank char
+                mov ah, 0x9
+                int 0x10 
+                mov ah, 02h
+                mov bh, 00
+                int 10h 
+                PRINT 177
+                
+                
+            draw_Greendoor:
+                ;object(door) spawn draw 
+                ;cursor pos:
+                mov dl,60 
+                mov dh,2
+                mov bh, 0
+                mov ah, 0x2
+                int 0x10
+                mov cx, 1 ; nb char
+                mov bh, 0
+                mov bl, 0x20 ; color
+                mov al, 0x20 ; blank char
+                mov ah, 0x9
+                int 0x10 
+                mov ah, 02h
+                mov bh, 00
+                int 10h 
+                PRINT 177       
+            
+           draw_hero:
+            
+
+              mov dl, 2 ; column
+              mov dh, 21 ; row 
+
+              call SetCursor
+        
+              PRINT ':)'
+          
+              
+             
+              
+            call SetCursor
+                mov cx, 1 ; nb char
+                mov bh, 0
+                mov bl, 0x40 ; color
+                mov al, 0x20 ; blank char
+                mov ah, 0x9
+                int 0x10 
+                mov ah, 02h
+                mov bh, 00
+                int 10h 
+                PRINT 177  
+     
+            jmp init_var
+    
+   
+    
+    
+     
+                  
